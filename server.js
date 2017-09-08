d1dfaf75dfabd97b8cdf70bec68f9044d2d3fcbe
mohamedElsonny <mohamedelsonny5@gmail.com>
2017-09-08 15:55:49 +0200

    Add project page

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.json({
         errorMessage: 'Bad Request'
